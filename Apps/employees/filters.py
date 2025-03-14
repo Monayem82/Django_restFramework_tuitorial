aae758673f8a0cd859d3ea34a616c154e2494b21
Monayem <mamonayem6@gmail.com>
2025-03-14 21:12:52 +0600

    custom Filtering

--- Apps/employees/filters.py
+++ Apps/employees/filters.py
@@ -2,9 +2,21 @@ import django_filters
 from .models import EmployeesModel
 
 class EmployeeFilter(django_filters.FilterSet):
-    designa =django_filters.CharFilter(field_name='designation',lookup_expr='iexact')
-    emp_names=django_filters.CharFilter(field_name='emp_name',lookup_expr='icontains')
+    designa =django_filters.CharFilter(field_name='designation',lookup_expr='iexact',label='Dasignaton emp')
+    emp_names=django_filters.CharFilter(field_name='emp_name',lookup_expr='icontains',)
+    #ids=django_filters.RangeFilter(field_name='id')
+    ids_min=django_filters.CharFilter(method='filter_by_id',label='id from')
+    ids_max=django_filters.CharFilter(method='filter_by_id',label='id to')
 
     class Meta:
         model=EmployeesModel
-        fields=['designa','emp_names']
+        fields=['designa','emp_names','ids_min','ids_max']
+
+    
+    def filter_by_id(self,queryset,name,value):
+        if name=="ids_min":
+            return queryset.filter(emp_id__gte=value)
+        elif name=="ids_max":
+            return queryset.filter(emp_id__lte=value)
+        
+        return queryset
